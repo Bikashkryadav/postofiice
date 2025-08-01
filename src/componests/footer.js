505d974ebaee22decd9535aa8f9708676ce0f488
Bikash kr yadav <bikasyadav73@gmail.com>
2025-08-01 17:00:25 +0545

    add footer and header

---
+++ src/componests/footer.js
@@ -0,0 +1,9 @@
+import "./footer.css"
+const Footer = () =>{
+    return(
+         <footer>
+            @ copyright 2023 postoffice
+         </footer>
+    );
+}
+export default Footer;
---
+++ src/componests/header.js
@@ -0,0 +1,21 @@
+import "./header.css"
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href='#'>home</a>
+                    </li>
+                    <li>
+                        <a href='#'>About</a>
+                    </li>
+                    <li>
+                        <a href='#'>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+export default Header;
